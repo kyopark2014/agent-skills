dce66f6510a17079b9b49e9291949fef17f15066
John Park <kyopark2014@gmail.com>
2026-03-19 13:19:25 +0900

    back weather credential

--- application/utils.py
+++ application/utils.py
@@ -121,6 +121,23 @@ else:
         region_name=bedrock_region
     )
 
+# api key for weather
+weather_api_key = ""
+try:
+    get_weather_api_secret = secretsmanager.get_secret_value(
+        SecretId=f"openweathermap-{projectName}"
+    )
+    #print('get_weather_api_secret: ', get_weather_api_secret)
+    secret = json.loads(get_weather_api_secret['SecretString'])
+    #print('secret: ', secret)
+    weather_api_key = secret['weather_api_key']
+    if weather_api_key:
+        os.environ["OPENWEATHERMAP_API_KEY"] = weather_api_key
+
+except Exception as e:
+    # raise e
+    pass
+
 # api key to use Tavily Search
 tavily_key = tavily_api_wrapper = ""
 try:
