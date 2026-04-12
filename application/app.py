9bcc0e0357eedbd7cdc1ba3ec525cab9792da6da
John Park <kyopark2014@gmail.com>
2026-04-12 20:45:51 +0900

    udpate

--- application/app.py
+++ application/app.py
@@ -119,7 +119,7 @@ with st.sidebar:
 
         # Change radio to checkbox        
         mcp_selections = {}
-        default_selections = ["web_fetch", "slack", "notion", "obsidian"]
+        default_selections = ["web_fetch", "slack", "notion"]
         
         with st.expander("MCP 옵션 선택", expanded=True):
             for option in mcp_options:
